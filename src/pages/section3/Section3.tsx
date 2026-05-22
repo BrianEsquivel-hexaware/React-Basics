7554cd3b9a4864cdd79aae806dbb965bb33e4c2b
2000145257 <BrianAlbertoE@hexaware.com>
2026-05-22 12:06:58 -0600

    Section 3 page component created

---
+++ src/pages/section3/Section3.tsx
@@ -0,0 +1,12 @@
+import "./Section3.css";
+
+function Section3() {
+  return (
+    <div className="section3">
+      <h1>Components</h1>
+      <p>Learn how to create reusable components in React.</p>
+    </div>
+  );
+}
+
+export default Section3;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import Home from '../pages/home/Home';
+import NotFound from '../pages/NotFound/NotFound';
 import Section1 from '../pages/section1/Section1';
 import Section2 from '../pages/section2/Section2';
-import NotFound from '../pages/NotFound/NotFound';
+import Section3 from '../pages/section3/Section3';
 
 export const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
       {
         path: '/section2',
         element: <Section2 />,
+      },
+      {
+        path: '/section3',
+        element: <Section3 />,
       },
       {
         path: '*',
