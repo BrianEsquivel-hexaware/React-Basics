929dc7d5521ebbd66614422a32e143e7d9a89808
2000145257 <BrianAlbertoE@hexaware.com>
2026-05-22 10:54:59 -0600

    Not found page added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,6 +9,14 @@ function Header() {
     { path: '/', label: 'Home' },
     { path: '/section1', label: 'Section 1' },
     { path: '/section2', label: 'Section 2' },
+    { path: '/section3', label: 'Section 3' },
+    { path: '/section4', label: 'Section 4' },
+    { path: '/section5', label: 'Section 5' },
+    { path: '/section6', label: 'Section 6' },
+    { path: '/section7', label: 'Section 7' },
+    { path: '/section8', label: 'Section 8' },
+    { path: '/section9', label: 'Section 9' },
+    { path: '/section10', label: 'Section 10' }
   ];
 
   const handleClose = () => setExpanded(false);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import Layout from '../layouts/Layout';
 import Home from '../pages/home/Home';
 import Section1 from '../pages/section1/Section1';
 import Section2 from '../pages/section2/Section2';
+import NotFound from '../pages/NotFound/NotFound';
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: '/section2',
         element: <Section2 />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
       },
     ],
   },
